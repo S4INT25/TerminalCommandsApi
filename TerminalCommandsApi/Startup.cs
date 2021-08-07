513f2fdc6b62480087635f701490ad1d968b01d2
saint <lucksonmwanambulo25@gmail.com>
2021-08-07 21:30:14 +0200

    moved back exception middleware

--- TerminalCommandsApi/Startup.cs
+++ TerminalCommandsApi/Startup.cs
@@ -92,9 +92,9 @@ namespace TerminalCommandsApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TerminalCommandsApi v1"));
             }
 
-           
-            app.UseHttpsRedirection();
             app.ConfigureCommanderExceptionHandler();
+            app.UseHttpsRedirection();
+           
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
